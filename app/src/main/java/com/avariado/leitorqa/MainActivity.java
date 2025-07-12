1dcbfb7e752d5c4083d712d2556e025e7170e71d
avariado <avariadopt@hotmail.com>
2025-07-12 23:55:12 +0100

    Update MainActivity.java

--- app/src/main/java/com/avariado/leitorqa/MainActivity.java
+++ app/src/main/java/com/avariado/leitorqa/MainActivity.java
@@ -17,6 +17,7 @@ import android.view.GestureDetector;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
